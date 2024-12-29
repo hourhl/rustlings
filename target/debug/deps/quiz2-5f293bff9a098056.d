25010c530dd425f026da70845f5dbedab705534d
hourhl <huilin8200@gmail.com>
2024-12-29 13:48:21 +0800

    update

---
+++ target/debug/deps/quiz2-5f293bff9a098056.d
@@ -0,0 +1,9 @@
+/home/xu/InfiniTensor/rustlings/target/debug/deps/libquiz2-5f293bff9a098056.rmeta: exercises/quizzes/quiz2.rs Cargo.toml
+
+/home/xu/InfiniTensor/rustlings/target/debug/deps/quiz2-5f293bff9a098056.d: exercises/quizzes/quiz2.rs Cargo.toml
+
+exercises/quizzes/quiz2.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
